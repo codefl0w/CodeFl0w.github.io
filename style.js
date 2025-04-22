e22188c0c61948bfed83dccf4d566d57838f2bae
fl0w <68522433+codefl0w@users.noreply.github.com>
2025-04-22 21:13:39 +0300

    Delete style.js

--- style.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.collapsible-section h2').forEach(header => {
-    header.addEventListener('click', () => {
-      const content = header.nextElementSibling;
-      content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-  });
